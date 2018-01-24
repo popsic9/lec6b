24429a792e7566e212d77232f48fc693030baf53
sunche <sunche@umich.edu>
2018-01-23 21:23:25 -0500

    Add 8ball.py

---
+++ 8ball.py
@@ -0,0 +1,12 @@
+import random
+
+ans_list = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+       "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
+       "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", 
+       "Better not tell you now", "Cannot predict now", "Concentrate and ask again"
+       "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
+       "Very doubtful"]
+
+que = input("Please enter your question. (Must ended with '?'): ")
+ans = random.choice(ans_list)
+print("The answer is: " + ans)
